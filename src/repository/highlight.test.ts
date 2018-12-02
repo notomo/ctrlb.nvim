cd36acca3fe00fb22f542c568ea6ed8384504897
notomo <notomo.motono@gmail.com>
2018-12-02 20:07:42 +0900

    Add tests

---
+++ src/repository/highlight.test.ts
@@ -0,0 +1,36 @@
+import { HighlightRepository } from "./highlight";
+import { Neovim } from "neovim";
+
+describe("HighlightRepository", () => {
+  let command: jest.Mock;
+
+  let highlightRepository: HighlightRepository;
+
+  beforeEach(() => {
+    command = jest.fn();
+    const NeovimClass = jest.fn<Neovim>(() => ({
+      command: command,
+    }));
+    const neovim = new NeovimClass();
+
+    highlightRepository = new HighlightRepository(neovim);
+  });
+
+  it("link", async () => {
+    const from = "from";
+    const to = "to";
+
+    await highlightRepository.link(from, to);
+    expect(command).toHaveBeenCalledWith(
+      `highlight default link ${from} ${to}`
+    );
+  });
+
+  it("match", async () => {
+    const name = "name";
+    const pattern = "pattern";
+
+    await highlightRepository.match(name, pattern);
+    expect(command).toHaveBeenCalledWith(`syntax match ${name} /${pattern}/`);
+  });
+});
