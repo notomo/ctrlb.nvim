7a8df8f3ea614625e53ee582e0427b8346df3ec4
notomo <notomo.motono@gmail.com>
2019-01-02 15:22:11 +0900

    Add wsxhub setup script

---
+++ src/setup/index.ts
@@ -0,0 +1,35 @@
+import { setupServerClient } from "./setup";
+import { sync as mkdir } from "mkdirp";
+import { chmodSync } from "fs";
+import { createReadStream, writeFileSync } from "fs";
+import { Extract } from "unzip";
+import { get } from "request";
+import { RequestCallback } from "request";
+
+const releaseTag = "v0.0.4";
+const binaryDirectory = "bin";
+
+const unarchive = (
+  zipPath: string,
+  binaryDirectory: string,
+  callback: Function
+) => {
+  createReadStream(zipPath)
+    .pipe(Extract({ path: binaryDirectory }))
+    .on("close", () => callback());
+};
+
+const getFile = (url: string, callback: RequestCallback) => {
+  get(url, { followAllRedirects: true, encoding: null }, callback);
+};
+
+setupServerClient(
+  process.platform,
+  releaseTag,
+  binaryDirectory,
+  mkdir,
+  chmodSync,
+  unarchive,
+  writeFileSync,
+  getFile
+);
